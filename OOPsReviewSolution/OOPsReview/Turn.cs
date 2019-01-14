b3965e73238fe74f550905c003f3ba9297c5966e
jbrower1 <46543431+jbrower1@users.noreply.github.com>
2019-01-14 16:53:52 -0700

    Created Turn and Turn EX

---
+++ OOPsReviewSolution/OOPsReview/Turn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Turn
+    {
+        // record one instance of each player rolling the die
+
+        // two ints
+        // player 1 player 2
+
+        // two int arrays
+
+        private string[] _Turns;
+        private int _TurnCount = 1;
+
+        public string[] Turns
+        {
+            get
+            {
+                return _Turns;
+            }
+        }
+       
+        public void AddTurn(int Player1Turn, int Player2Turn)
+        {
+            _Turns[_TurnCount] =(Player1Turn + " " + Player2Turn + " ");
+            _TurnCount ++;
+        }
+    }
+}
---
+++ OOPsReviewSolution/OOPsReview/TurnEX.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    class TurnEX
+    {
+        private int _Player1Roll;
+
+        public int Player1Roll
+        {
+            get
+            {
+                return _Player1Roll;
+            }
+            set
+            {
+                _Player1Roll = value;
+            }
+        }
+
+        public int Player2Roll { get; set; }
+
+        public TurnEX()
+        {
+
+        }
+        public TurnEX(int Player1,int Player2)
+        {
+            Player1Roll = Player1;
+            Player2Roll = Player2;
+        }
+
+        //methods 
+        //public String findRollResults()
+        //{
+        //  return null;
+        //}
+    }
+}
