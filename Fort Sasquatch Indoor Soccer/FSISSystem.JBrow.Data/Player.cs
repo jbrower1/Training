910977cec5c331b769aa0a637da58e5d3f78d378
jbrower1 <46543431+jbrower1@users.noreply.github.com>
2019-03-18 16:07:59 -0600

    Revert "Revert "Merge branch 'master' of https://github.com/jbrower1/Training""
    This reverts commit e5a426dbe093d9ac395703c07cb2f7091c33ce58.

---
+++ Fort Sasquatch Indoor Soccer/FSISSystem.JBrow.Data/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+#endregion
+
+namespace FSISSystem.JBrow.Data
+{
+    [Table("Player")]
+    public class Player
+    {
+        [Key]
+        public int PlayerID { get; set; }
+
+        [Key, Column(Order = 2)]
+        public int GuardianID { get; set; }
+
+        [Key, Column(Order = 3)]
+        public int TeamID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public char Gender
+        {
+            get
+            {
+                return Gender;
+            }
+            set
+            {
+                Gender = char.ToUpper(value);
+            }
+        }
+
+        public string AlbertaHealthCareNumber { get; set; }
+
+        public string MedicalAlertDetails
+        {
+            get
+            {
+                return MedicalAlertDetails;
+            }
+            set
+            {
+                if(value == "")
+                {
+                    MedicalAlertDetails = null;
+                }
+                
+            }
+        }
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
