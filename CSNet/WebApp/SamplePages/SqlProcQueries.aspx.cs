315e6043291a540a46dbd4eac62812e328b9b90d
jbrower1 <46543431+jbrower1@users.noreply.github.com>
2019-03-20 15:36:57 -0600

    install startup kit, load ddl control

---
+++ CSNet/WebApp/SamplePages/SqlProcQueries.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional Namespaces
+using NorthwindSystem.BLL;  //points to the controller class
+using NorthwindSystem.Data; //points to the record descriptions
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class SqlProcQueries : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // clear old messages
+            MessageLabel.Text = "";
+
+            //load the drop down list on the first time processing this page
+            //all calls should be done in user friendly error handling
+            if (!Page.IsPostBack)
+            {
+
+
+                try
+                {
+
+                    //when the page is first loaded, obtain the complete list of categories from the database
+
+                    CategoryController sysmgr = new CategoryController();
+
+                    List<Category> datainfo = sysmgr.Category_List();
+
+                    //sort this list alphabetically
+                    datainfo.Sort((x, y) => x.CategoryName.CompareTo(y.CategoryName));
+                    //assign the data to the drop down list control
+                    CategoryList.DataSource = datainfo;
+                    //indicate the DataTextField and DataValueField
+                    CategoryList.DataTextField = nameof(Category.CategoryName);
+                    CategoryList.DataValueField = nameof(Category.CategoryID);
+                    //bind the resource
+                    CategoryList.DataBind();
+                    //add a prompt
+                    CategoryList.Items.Insert(0, "select ...");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageLabel.Text = ex.Message;
+                }
+            }
+        }      
+    }
+}
